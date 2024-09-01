fd53f48a4395d4c6181f1027f9e95539312e52d4
Lucas Finoti Rodrigues <lucas-finoti@lucas.rodrigues>
2024-09-01 15:29:48 -0300

    feat: content in disciplinas added to the modal

--- guiamochileirofatecano/src/main/webapp/app/entities/disciplinas/detail/disciplinas-detail.component.ts
+++ guiamochileirofatecano/src/main/webapp/app/entities/disciplinas/detail/disciplinas-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IDisciplinas } from '../disciplinas.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-disciplinas-detail',
@@ -9,11 +10,19 @@ import { IDisciplinas } from '../disciplinas.model';
 })
 export class DisciplinasDetailComponent implements OnInit {
   disciplinas: IDisciplinas | null = null;
-  disciplina: any;
+  disciplinaSelcionada: any;
   
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(
+    protected activatedRoute: ActivatedRoute,
+    private modalService: NgbModal
+  ) {}
+
+  close():void {
+    this.modalService.dismissAll();
+  }
 
   ngOnInit(): void {
+    console.warn("disciplinaSelcionada => ", this.disciplinaSelcionada);
     this.activatedRoute.data.subscribe(({ disciplinas }) => {
       this.disciplinas = disciplinas;
     });
